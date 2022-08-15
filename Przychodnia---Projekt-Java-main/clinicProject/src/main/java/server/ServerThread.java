a660e55c04ea57e5b9bb55d0a533c806938d2c61
Witold Weiner <42048512+witke16@users.noreply.github.com>
2022-08-15 03:42:46 +0200

    change type of sql, some work at api

---
+++ Przychodnia---Projekt-Java-main/clinicProject/src/main/java/server/ServerThread.java
@@ -0,0 +1,66 @@
+package server;
+// polaczenie serwer klient
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import database.DBconfig.DBAPI;
+import database.tables.Doctors;
+
+import java.io.*;
+import java.net.Socket;
+import com.fasterxml.jackson.*;
+public class ServerThread implements Runnable{
+
+     Socket socket;
+    String clientMessage;
+    ObjectNode jsonNode;
+    BufferedReader inputStream ;
+    PrintStream outputStream ;
+    DBAPI dbapi;
+    public ServerThread(Socket socket) throws IOException {
+        try {
+            this.socket = socket;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public void run () {
+        System.out.println("* ------ *");
+        System.out.println("[ Usluga ]: polaczenia z: " + socket.getInetAddress() + "/" + socket.getPort());
+
+        try {
+           inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            outputStream = new PrintStream(socket.getOutputStream());
+            dbapi = new DBAPI();
+
+            clientCommunication(); // Otwieramy mozliwosc wymieniania informacji z clientem
+
+            outputStream.close();
+            inputStream.close();
+            socket.close();
+        }
+        catch (Exception e) {
+            System.err.println("[ Usluga ]: Exception: " + e);
+        }
+        System.out.println("[ Usluga ]: rozlaczanie: " + socket.getInetAddress () + "/" + socket.getPort ());
+        System.out.println("* ------ *");
+    }
+
+   private void clientCommunication() throws IOException
+   {
+        /* czy sprawdzamy odrazu status accesu? */
+       /*czy szukamy cos na pierwsze spojrzenie? */
+
+       /*petla obslugujaca zapytania */
+       while (true)
+       {
+           try {
+               clientMessage = inputStream.readLine();
+           } finally {
+           }
+       }
+
+   }
+
+}
---
+++ Przychodnia---Projekt-Java-main/clinicProject/src/main/java/server/ServiceHTTP.java
@@ -0,0 +1,43 @@
+package server;
+// polaczenie serwer klient
+import java.io.*;
+import java.net.Socket;
+
+public class ServiceHTTP implements Runnable {
+
+    Socket socket;
+    public ServiceHTTP (Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run () {
+        System.out.println("* ------ *");
+        System.out.println("[ Usluga ]: polaczenia z: " + socket.getInetAddress() + "/" + socket.getPort());
+
+        try {
+            BufferedReader inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintStream outputStream = new PrintStream(socket.getOutputStream());
+            String JsonToClient = "";
+
+                // wstepnie tylko wyslanie przykladowego obiektu jako json i odczyt jego w kliencie
+            JsonDemo.JsonDemoServer();
+
+
+            outputStream.println("HTTP/1.0 200 OK\r\n");
+            outputStream.println( JsonToClient );
+
+            socket.close();
+        }
+        catch (Exception e) {
+            System.err.println("[ Usluga ]: Exception: " + e);
+        }
+
+        System.out.println("[ Usluga ]: rozlaczanie: " + socket.getInetAddress () + "/" + socket.getPort ());
+        System.out.println("* ------ *");
+    }
+
+    private String getServerResult(String userCommand, String userParams){
+        return "dupa";
+    }
+
+}
