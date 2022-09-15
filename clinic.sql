edf6fcead0f8234dbb71ad5a0ef77443c5775484
Mateusz Szymczak <84203463+mszymczak710@users.noreply.github.com>
2022-09-15 09:33:47 +0200

    Update clinic.sql

--- clinic.sql
+++ clinic.sql
@@ -11,9 +11,10 @@ CREATE TABLE Patients (
 	zip_code VARCHAR(6),
 	phone_number VARCHAR(9) NOT NULL,
 	email_address VARCHAR(30),
-	pass VARCHAR(15) NOT NULL,
+	password VARCHAR(15) NOT NULL,
 	PRIMARY KEY (patient_id)
 );
+
 
 INSERT INTO Patients VALUES (1, '97062064754', 'Mateusz', 'Kowalski', '1997-06-20', 'Warynskiego 13', 'Torun', '87-100', '728534121', 'matikowalski@interia.pl', '123');
 INSERT INTO Patients VALUES (2, '58012659938', 'Eugeniusz', 'Walczak', '1958-01-26', 'Mickiewicza 132/12', 'Torun', '87-100', '542324367', NULL, '123');
@@ -21,21 +22,19 @@ INSERT INTO Patients VALUES (3, NULL, 'Denys', 'Kovalenko', '2000-03-23', 'Micki
 INSERT INTO Patients VALUES (4, '02262054621', 'Nikola', 'Jankowska', '2002-06-20', 'Mostowa 13/1', 'Torun', '87-100', '608244154', 'nikijankowska@onet.pl', '123');
 INSERT INTO Patients VALUES (5, '67051039999', 'Eustachy', 'Wozniak', '1967-05-10', 'Waniliowa 15', 'Torun', '87-100', '575354102', NULL, '123');
 
-
 CREATE TABLE Doctors (
 	doctor_id INT NOT NULL,
 	first_name VARCHAR(20) NOT NULL,
 	last_name VARCHAR(20) NOT NULL,
 	job_execution_number INT NOT NULL UNIQUE,
 	specialization VARCHAR(40),
-	pass VARCHAR(15) NOT NULL,
+	password VARCHAR(15) NOT NULL,
 	PRIMARY KEY (doctor_id)
 );
 
 INSERT INTO Doctors VALUES (1, 'Jan', 'Kowalski', 654321, 'internista', '123');
 INSERT INTO Doctors VALUES (2, 'Maria', 'Borowska', 742148, 'ginekolog', '123');
 INSERT INTO Doctors VALUES (3, 'Elzbieta', 'Kalinowska', 854317, 'pediatra', '123');
-
 
 
 CREATE TABLE Visits (
@@ -51,6 +50,7 @@ CREATE TABLE Visits (
 INSERT INTO Visits VALUES (1, '2022-06-30 12:00:00', 30, 2, 1, 3);
 INSERT INTO Visits VALUES (2, '2022-06-30 10:00:00',45 , 4, 2, 2);
 
+
 CREATE TABLE Offices (
 	office_number INT NOT NULL,
 	type_of_office VARCHAR(50),
@@ -61,6 +61,7 @@ INSERT INTO Offices VALUES (1, 'chorob wewnetrznych');
 INSERT INTO Offices VALUES (2, 'ginekologiczny');
 INSERT INTO Offices VALUES (3, 'chorob wewnetrznych');
 INSERT INTO Offices VALUES (4, 'pediatryczny');
+
 
 CREATE TABLE Prescriptions (
 	prescription_id INT NOT NULL,
@@ -80,7 +81,6 @@ ALTER TABLE Visits ADD CONSTRAINT Visits_fk1 FOREIGN KEY (doctor_id) REFERENCES 
 ALTER TABLE Visits ADD CONSTRAINT Visits_fk2 FOREIGN KEY (office_number) REFERENCES Offices(office_number);
 
 ALTER TABLE Prescriptions ADD CONSTRAINT Prescriptions_fk0 FOREIGN KEY (visit_id) REFERENCES Visits(visit_id);
-
 
 
 END;
